8558231be559399be43b0260656b8947491f8112
elianeferreira <sanelisantos@hotmail.com>
2023-04-27 20:39:04 -0300

    exercicio calculo da nota - semana5-javascript

---
+++ OneDrive/Área de Trabalho/FRONTEND/Semana5/Aula 1/Ex1.js
@@ -0,0 +1,7 @@
+let valorReais = prompt (" Qual o valor em reais a ser convertido para dólar?")
+
+let taxaDeCambio = 5.30
+
+let resultado = valorReais / taxaDeCambio
+alert('valor convertido em dolar é + '+ valorReais /taxaDeCambio)
+
